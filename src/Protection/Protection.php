6a6e34b616569aca13af6cfaaf3439c89aab9e80
ksc2650 <ksc2650@rit.edu>
2016-11-23 14:22:22 -0500

    added isCredentialValid as well as a method to return the data that was fetched

--- src/Protection/Protection.php
+++ src/Protection/Protection.php
@@ -15,7 +15,12 @@ class Protection
 {
     private $sandbox = false;
     private $ip = '127.0.0.1';
+    private $associateLoginData = null;
 
+    /**
+     * Protection constructor -this create a protection constructor and return the instance.
+     * The constructor checks if the session has started, if not it automatically start it
+     */
     public function __construct()
     {
         if (session_status() == PHP_SESSION_NONE)
@@ -61,12 +66,24 @@ class Protection
         }
     }
 
+    /**
+     * @param $database
+     * @param $table
+     * @param $username
+     * @param $password
+     * @param $usernameCol
+     * @param $passwordCol
+     * @return bool|\stdClass
+     */
     public function secureCreateUserAccount($database,$table,$username,$password,$usernameCol,$passwordCol)
     {
         $this->validate_secure_create_user_account($database,$table,$username,$password,$usernameCol,$passwordCol);
 
         if($database instanceof Database)
         {
+            //the username acts as nounce in this case
+            $password = $this->generateSecurePassword($username,$password);
+
             $columns = $usernameCol.','.$passwordCol;
             $bindings = array(':username'=>$username,':password'=>$password);
             $values = ':username,:password';
@@ -79,12 +96,22 @@ class Protection
         return false;
     }
 
+    /**
+     * This method is use to enable the sandbox.
+     * The ip is also set to localhost 127.0.0.1
+     */
     public function enableSandbox()
     {
         $this->sandbox = true;
         $this->ip = '127.0.0.1';
     }
 
+    /**
+     * Take an object and return a boolean expression whether it is empty or not.
+     * Capable of checkng string, nullable, array
+     * @param $object - the object to check for empty
+     * @return bool - true if the object is empty and false otherwise
+     */
     public function isEmpty($object)
     {
         if(!isset($object))
@@ -105,6 +132,15 @@ class Protection
         return false;
     }
 
+    /**
+     * This method is use to generate a cryptographically secured password. It takes a nounce, password(plaintext) and options
+     * then use the php password_hash function along with PASSWORD_BCRYPT to returned a secured hash.
+     * An InvalidArgumentException is thrown if password is empty or options is not an array
+     * @param string $nounce - the nounce, username can be use for this
+     * @param $password - the user's plaintext password
+     * @param array $options
+     * @return bool|string return a string of hashed password if succeed False if failed.
+     */
     public function generateSecurePassword($nounce="",$password,$options = array('cost'=>12))
     {
         if($this->isEmpty($password))
@@ -121,6 +157,13 @@ class Protection
         return password_hash($combination,PASSWORD_BCRYPT,$options);
     }
 
+
+    /**
+     * This method is used to trim the ip into its appropriate format.
+     * Throw an InvalidArgumentException if the ip is empty
+     * @param $ip - the ip in string format
+     * @return string - returns a string format of the trimmed ip
+     */
     public function trimIP($ip)
     {
         if($this->isEmpty($ip))
@@ -137,7 +180,13 @@ class Protection
         return $ip.'.0';
     }
 
-    function validate_ip($ip)
+    /**
+     * This method takes a string of ip and returns whether it is validate or not.
+     * Throw an InvalidArgumentException ip is empty
+     * @param $ip
+     * @return bool
+     */
+    public function validate_ip($ip)
     {
         if($this->isEmpty($ip))
         {
@@ -148,6 +197,42 @@ class Protection
             return false;
         }
         return true;
+    }
+
+    /**
+     * This method returns the data of the user that is successful login from the database.
+     * Call this method after the isCredentialValid method has return true
+     * @return return the associated data otherwise return null
+     */
+    public function getSuccessfulLoginData()
+    {
+        return $this->associateLoginData;
+    }
+    public function isCredentialValid($database,$username,$password,$table,$columns,$where,$bindings)
+    {
+        if(!isset($database) || !$database instanceof Database)
+        {
+            throw new \InvalidArgumentException("databasse parameter must be an instance of Database!");
+        }
+
+        if($this->isEmpty($username) || $this->isEmpty($password) || $this->isEmpty($table)||
+            $this->isEmpty($columns) || $this->isEmpty($where) || $this->isEmpty($bindings))
+        {
+            throw new \InvalidArgumentException("all parameters are required!");
+        }
+
+        if($database instanceof Database)
+        {
+            $res = $database->select($table,$columns,$where,$bindings);
+            if($res instanceof \PDOStatement)
+            {
+
+                $this->associateLoginData = $res->fetch();
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public function get_ip_address()
